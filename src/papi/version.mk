96678865d196131573174f1c8d3d9b1d9d80c5db
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 11:20:05 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/papi/version.mk
+++ src/papi/version.mk
@@ -1,3 +1,14 @@
-NAME    = papi
-VERSION = sandybridge_rapl
-RELEASE = 1
+NAME               = papi
+VERSION            = sandybridge_rapl
+RELEASE            = 1
+PKGROOT            = /opt/papi
+
+SRC_SUBDIR         = papi
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
